eb5a9f227e3f04b0dfdc4debf5039b791c7335a6
Logarithmic <omega_x86@outlook.com>
2025-11-14 22:27:01 +0800

    初始化脚本提权

--- FFXIV_Init.bat
+++ FFXIV_Init.bat
@@ -2,22 +2,22 @@
 setlocal EnableDelayedExpansion
 
 :: 提权检查
-@REM >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-@REM if '%errorlevel%' NEQ '0' (
-@REM     echo 请求管理员权限...
-@REM     goto UACPrompt
-@REM ) else ( goto gotAdmin )
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo 请求管理员权限...
+    goto UACPrompt
+) else ( goto gotAdmin )
 
-@REM :UACPrompt
-@REM     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-@REM     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-@REM     "%temp%\getadmin.vbs"
-@REM     exit /B
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
 
-@REM :gotAdmin
-@REM     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-@REM     pushd "%CD%"
-@REM     CD /D "%~dp0"
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
 
 :: 主脚本开始
 echo ========================================
